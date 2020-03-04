e88dd6599ea0e415d43317ab9f678bbcca88e67e
TeMecha Newton <tanewton0523@gmail.com>
2020-03-04 19:23:45 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,12 +2,12 @@ def meal_choice(veg1 = 'broccoli', veg2 = 'macaroni', protein = 'roast beef')
   puts "What a nutritious meal!"
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
   puts meal
-  meal
+  return meal
 end
 
 def meal_choice(veg1 = 'broccoli', veg2 = 'macaroni', protein = 'tofu')
   puts "What a nutritious meal!"
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
   puts meal
-  meal
+  return meal
 end
